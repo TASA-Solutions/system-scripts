5f22ca802824ccabbb16cc5bb09b29c9513fd30a
arshadazaad3 <arshad.azaad32@gmail.com>
2023-04-04 22:48:19 +0530

    updated network info script with interval of 5 seconds

--- cron/network_stat/network_info.sh
+++ cron/network_stat/network_info.sh
@@ -1,19 +1,38 @@
 #!/usr/bin/bash
 
-mkdir stat;
-mkdir fail;
-( echo "`date` : "; uname -a;  free -h;fdisk -l; ) >> stat/"wifi_stat_$(date +%F)".txt;
+# echo $USER_PATH
+
+DESKTOP_DIR="$HOME/Desktop"
+PARENT_DIR="$DESKTOP_DIR/wifi_stat"
+STAT_DIR="$DESKTOP_DIR/wifi_stat/stat"
+ERROR_DIR="$DESKTOP_DIR/wifi_stat/error"
+
+mkdir -p "$PARENT_DIR"
+mkdir -p "$STAT_DIR"
+mkdir -p "$ERROR_DIR"
+
+while true
+do
+  ( echo "==========================================================";) >> "$STAT_DIR/$(date +%F)".txt;
+  # ( echo "`date` : "; uname -a;  free -h;fdisk -l; ) >> stat/"$(date +%F)".txt;
+  ( echo "`date` : ";) >> "$STAT_DIR/$(date +%F)".txt;
 
 
-( ping -c 1 www.google.com ) >> stat/"wifi_stat_$(date +%F)".txt;
-# ( curl https://sharedapi.tasa.com.my/health ) >> stat/"wifi_stat_$(date +%F)".txt;
-( echo; ) >> stat/"wifi_stat_$(date +%F)".txt;
+  ( ping -c 1 www.google.com ) >> "$STAT_DIR/$(date +%F)".txt;
+  # ( curl https://sharedapi.tasa.com.my/health ) >> "$STAT_DIR/$(date +%F)".txt;
+  # ( echo; ) >> "$STAT_DIR/$(date +%F)".txt;
 
-if ping -c 1 www.google.com &> /dev/null
-then
-  echo "success"
-else
-  echo "error"
-  ( echo "`date` : "; ) >> fail/"ping_fail_$(date +%F)".txt;
-  ( ping -c 1 www.google.com; echo; ) >> fail/"ping_fail_$(date +%F)".txt;
-fi
+  if ping -c 1 www.google.com &> /dev/null
+  then
+    echo "success"
+  else
+    echo "error"
+    ( echo "`date` : "; ) >> "$ERROR_DIR/ping_fail_$(date +%F)".txt;
+    ( ping -c 1 www.google.com ) >> "$ERROR_DIR/ping_fail_$(date +%F)".txt;
+  fi
+
+  ( echo "==========================================================";) >> "$STAT_DIR/$(date +%F)".txt;
+  ( echo;) >> "$STAT_DIR/$(date +%F)".txt;
+  
+  sleep 5
+done
